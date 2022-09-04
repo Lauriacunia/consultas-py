2801aec54d17273ea243d6c0547d1b7f8f33d2b9
Lauriacunia <lauriacunia@gmail.com>
2022-09-04 14:29:22 -0300

    subo consultas del 4 sep

---
+++ consultas-4sep.py
@@ -0,0 +1,7 @@
+# 1-para agregar un elemento a un diccionario existe alguna diferencia entre usar
+d = {'uno': 1, 'dos': 2, 'tres': 3}
+d['cuatro'] = 4.0
+# o usar? tiene alguna aplicacion practica?
+d.setdefault('cuatro', 4.0)
+
+# 2-Asi como en Js se utilizan diferentes stacks como MERN ...Hay algún stack de python?
